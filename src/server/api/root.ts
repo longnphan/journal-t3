f24864979b1d4e7a2bba5f486a88557ae977a389
Long Phan <longnphan@outlook.com>
2023-09-24 14:29:47 -0600

    update /src/server/api/root.ts

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,5 +1,4 @@
 import { createTRPCRouter } from "./trpc";
-import { exampleRouter } from "./routers/example";
 import { noteRouter } from "./routers/note";
 
 /**
@@ -8,7 +7,6 @@ import { noteRouter } from "./routers/note";
  * All routers added in /api/routers should be manually added here
  */
 export const appRouter = createTRPCRouter({
-  example: exampleRouter,
   note: noteRouter,
 });
 
